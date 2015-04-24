074d94f471b1fd7e1f99000eecc11c3446eb6f71
Jon Stevens <latchkey@gmail.com>
2015-04-23 17:55:53 -0700

    fix paths

--- bin/boot.sh
+++ bin/boot.sh
@@ -7,16 +7,17 @@ erb_function() {
 
 echo "Processing erb files..."
 export -f erb_function
-find /app/apache/conf -type f -name "*.erb" | xargs -n 1 bash -c 'erb_function "$@"'
+find /app/apache/etc/apache2 -type f -name "*.erb" | xargs -n 1 bash -c 'erb_function "$@"'
 
 for var in `env|cut -f1 -d=`; do
 	echo "PassEnv \$var" >> /app/apache/conf/httpd.conf;
 done
 
+mkdir -p /app/apache/logs
 touch /app/apache/logs/error_log
 touch /app/apache/logs/access_log
 tail -F /app/apache/logs/error_log &
 tail -F /app/apache/logs/access_log &
 
 echo "Launching apache..."
-exec /app/apache/bin/httpd -DNO_DETACH
+exec /app/apache/sbin/httpd -DNO_DETACH
